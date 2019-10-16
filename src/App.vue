319855fd1870b25b1b45a8fb1e2e6ed7dfa7b9a9
Santiago Ramirez Castaño <santiramirezc@gmail.com>
2019-10-16 17:04:02 -0500

    Template inicial Vue + Router + Materialize

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header></Header>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+  export default {
+    mounted() {
+      M.AutoInit(); // That way, it is only initialized when the component is mounted
+      
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.collapsible');
+        var instances = M.Collapsible.init(elems, {});
+      });
+    },created(){
+      let icons = document.createElement('link');    
+      icons.setAttribute('href',"https://fonts.googleapis.com/icon?family=Material+Icons");
+      icons.setAttribute('rel',"stylesheet");
+      document.head.appendChild(icons);
+    },
+    components: {
+      Header,
+      Footer
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import '/css/materialize.min.css';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <nav>
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+            </ul>
+            </div>
+        </nav>
+    </div>
+</template>
